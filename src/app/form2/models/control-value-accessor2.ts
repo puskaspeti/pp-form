d80932bcd1a0e0284dabe586a9cad48d60b75e04
Puskás Péter <puskas.peter@nextval.hu>
2020-06-11 17:35:55 +0200

    fix unnecessary any cast

--- src/app/form2/models/control-value-accessor2.ts
+++ src/app/form2/models/control-value-accessor2.ts
@@ -1,4 +1,4 @@
-import { ControlValueAccessor } from '@angular/forms';
+import { AbstractControl, ControlValueAccessor } from '@angular/forms';
 import { FormControl2 } from './form-control2';
 import { FormControl2Options } from './form-control2-options';
 import { NgControl2 } from './ng-control2';
@@ -12,12 +12,11 @@ export class ControlValueAccessor2<
   ngControl: NgControl2;
   disabled = false;
 
-  get control(): TControl {
-    // TODO: fix 'any' cast
-    return this.ngControl?.control as any;
+  get control(): AbstractControl | TControl | null {
+    return this.ngControl?.control;
   }
 
-  get options(): TOptions {
+  get options(): TOptions | null {
     if (this.control instanceof FormControl2) {
       return this.control.options;
     }
