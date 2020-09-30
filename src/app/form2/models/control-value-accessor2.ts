cc3888d565217f2d4d49037a3f0dfbe0df303d63
Puskás Péter <puskas.peter@nextval.hu>
2020-09-30 10:42:23 +0200

    CVA without life cycle hooks

--- src/app/form2/models/control-value-accessor2.ts
+++ src/app/form2/models/control-value-accessor2.ts
@@ -1,6 +1,5 @@
-import { Directive, Input, OnDestroy, OnInit } from '@angular/core';
-import { AbstractControl, ControlValueAccessor, FormControl } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { Directive, Input, Type } from '@angular/core';
+import {AbstractControl, ControlValueAccessor, FormControl} from '@angular/forms';
 import { FormControl2 } from './form-control2';
 import { FormControl2Options } from './form-control2-options';
 import { NgControl2 } from './ng-control2';
@@ -10,9 +9,8 @@ export abstract class ControlValueAccessor2<
   TValue = unknown,
   TOptions extends FormControl2Options = FormControl2Options,
   TControl extends FormControl2<TValue, TOptions> = FormControl2<TValue, TOptions>,
-> implements ControlValueAccessor, OnInit, OnDestroy {
-
-  control: AbstractControl | TControl | null;
+  TAlma extends new() => TAlma = null
+> implements ControlValueAccessor {
 
   @Input() set options(val: TOptions) {
     if (this.control instanceof FormControl2) {
@@ -28,33 +26,37 @@ export abstract class ControlValueAccessor2<
     return this._options;
   }
 
+  get control(): AbstractControl | TControl | null {
+    if (this.ngControl) {
+      return this.ngControl.control;
+    }
+
+    if (!this.defaultControl) {
+      this.setUpDefaultControl();
+    }
+
+    return this.defaultControl;
+  }
+
+  set control(val: AbstractControl | TControl) {
+    this.defaultControl = val;
+  }
+
   ngControl: NgControl2;
   disabled = false;
 
+  protected defaultControl: AbstractControl;
   // tslint:disable-next-line:variable-name
   protected _options: TOptions;
-  protected subscription: Subscription;
 
   onChange = (value: TValue) => {};
   onTouched = () => {};
   onWriteValue = (value: TValue) => {};
 
-  ngOnInit(): void {
-    if (this.ngControl) {
-      this.control = this.ngControl.control;
-    } else {
-      this.setUpDefaultControl();
-    }
-  }
-
-  ngOnDestroy(): void {
-    this.subscription?.unsubscribe();
-  }
-
   setUpDefaultControl(): void {
     this.control = new FormControl();
     this.onWriteValue = (value) => { this.control.setValue(value, { emitViewToModelChange: false }); };
-    this.subscription = this.control.valueChanges.subscribe((value) => {
+    this.control.valueChanges.subscribe((value) => {
       this.onChange(value);
     });
   }
