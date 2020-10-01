d4410ed694952e1fa53a2dd5dc4910f0548cfd8c
Puskás Péter <puskas.peter@nextval.hu>
2020-10-01 13:09:27 +0200

    formGroup fix

--- src/app/form2/models/generic-types.ts
+++ src/app/form2/models/generic-types.ts
@@ -1,5 +1,7 @@
 import {AbstractControl2} from './abstract-control2';
 import {FormArray2} from './form-array2';
+import {FormControl2} from './form-control2';
+import {FormGroup2} from './form-group2';
 
 export type SingleOrArrayType<TValue> = TValue | TValue[];
 export type RequiredType<TValue> = TValue extends object ? Required<TValue> : TValue;
@@ -8,6 +10,6 @@ export type BaseType<TType> = TType extends (infer TArrayElementType)[] ? TArray
 
 export type FormGroupControlsType<TValue> = {
   [P in keyof Required<TValue>]: TValue[P] extends any[]
-    ? FormArray2<TValue[P]>
-    : AbstractControl2<BaseType<TValue[P]>>
+    ? FormArray2<TValue[P]> | FormControl2<TValue[P]>
+    : FormGroup2<TValue[P]> | FormControl2<TValue[P]>
 };
